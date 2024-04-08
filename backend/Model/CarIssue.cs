c9ccf9b2f6659e2a338109bb0fe992f21f9bcb78
Krzysztof Żywiecki <krzysztof.zywiecki@gmail.com>
2024-04-08 13:43:08 +0200

    Updated CarIssueDTO to contain ID. Added UpdateCarIssueDTO

--- backend/Model/CarIssue.cs
+++ backend/Model/CarIssue.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Backend.Models;
 
@@ -16,22 +17,36 @@ public class CarIssue
     [Column(TypeName = "decimal(8, 2)")]
     public decimal? RepairCost { get; set; }
 
-    public Car? Car { get; set; }
     public Guid CarId { get; set; }
 };
 
-public record CreateCarIssueDTO(string Description, long CarId);
+public record CreateCarIssueDTO(
+    string Title,
+    string Description,
+    DateTime CreateDateTime);
 
 public record CarIssueDTO(
+    Guid Id,
+    string Title,
     string Description,
     DateTime CreateDateTime,
     DateTime? RepairDateTime,
     decimal? RepairCost)
 {
     public CarIssueDTO(CarIssue issue) : this(
+        Id: issue.Id,
+        Title: issue.Title,
         Description: issue.Description,
         CreateDateTime: issue.CreateDateTime,
         RepairDateTime: issue.RepairDateTime,
         RepairCost: issue.RepairCost)
     { }
 }
+
+public record UpdateCarIssueDTO(
+    string Title,
+    string Description,
+    DateTime CreateDateTime,
+    DateTime? RepairDateTime,
+    decimal? RepairCost
+);
