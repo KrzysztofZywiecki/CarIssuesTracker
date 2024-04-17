343a06f964158bab7b33f63ab56eb141a39afd51
Krzysztof Żywiecki <krzysztof.zywiecki@gmail.com>
2024-04-17 14:36:37 +0200

    Fixed interceptor not retrying when two unauthorized requests were fired simultaneously

--- frontend/src/app/auth/helpers/auth.interceptor.ts
+++ frontend/src/app/auth/helpers/auth.interceptor.ts
@@ -6,7 +6,19 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 
-import { Observable, catchError, retry, switchMap, throwError } from "rxjs";
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  concatMap,
+  filter,
+  finalize,
+  of,
+  retry,
+  switchMap,
+  take,
+  throwError,
+} from "rxjs";
 import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
 
@@ -15,42 +27,61 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private _authService: AuthService, private _router: Router) {}
 
   isRefreshing: boolean = false;
+  tokenRefreshed: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     if (this._authService.accessToken !== null) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${this._authService.accessToken}`,
-        },
-      });
+      req = this.addAccessToken(req);
     }
     return next.handle(req).pipe(
       catchError((error) => {
-        if (error.status == 401 && !this.isRefreshing) {
-          this.isRefreshing = true;
-          return this._authService.refreshTokens().pipe(
-            switchMap(() => {
-              this.isRefreshing = false;
-              req = req.clone({
-                setHeaders: {
-                  Authorization: `Bearer ${this._authService.accessToken}`,
-                },
-              });
-              return next.handle(req);
-            }),
-            catchError((err) => {
-              this.isRefreshing = false;
-              this._router.navigate(["auth"]);
-              throw err;
-            })
-          );
+        if (error.status == 401) {
+          return this.refreshTokens(req, next);
         } else {
           throw error;
         }
       })
     );
+  }
+
+  private addAccessToken(req: HttpRequest<any>) {
+    return req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this._authService.accessToken}`,
+      },
+    });
+  }
+
+  private refreshTokens(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<any> {
+    if (this.isRefreshing) {
+      return this.tokenRefreshed.pipe(
+        filter((x) => x),
+        take(1),
+        concatMap(() => next.handle(this.addAccessToken(req)))
+      );
+    } else {
+      this.isRefreshing = true;
+      this.tokenRefreshed.next(false);
+
+      return this._authService.refreshTokens().pipe(
+        concatMap((_) => {
+          this.tokenRefreshed.next(true);
+          return next.handle(this.addAccessToken(req));
+        }),
+        catchError((err) => {
+          this._authService.logOut();
+          throw err;
+        }),
+        finalize(() => {
+          this.isRefreshing = false;
+        })
+      );
+    }
   }
 }
